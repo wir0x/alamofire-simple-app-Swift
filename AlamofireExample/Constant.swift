5a9c9bcbbd64b103b5c50344679212c9fdd1cd2e
wir0x <gsalazar-v@outlook.com>
2016-02-09 19:16:50 -0400

    simple app consuming rest service with alamofire

---
+++ AlamofireExample/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  AlamofireExample
+//
+//  Created by Gonzalo Salazar Velasquez on 2/9/16.
+//  Copyright © 2016 Gonzalo Salazar Velasquez. All rights reserved.
+//
+
+struct Constant {
+    static let danielURL = "http://algoritmo.16mb.com/ws/api/inquilinos/inquilino/2"
+    static let buhoStatusURL = "https://app.buho.bo/rest/app/status"
+}
--- AlamofireExample/ViewController.swift
+++ AlamofireExample/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // code using alamofire
+        Alamofire.request(.GET, Constant.danielURL)
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
